dfb70b984660d7dbaae9390415f43ddbe13c6059
Eric Biewener <eric.biewener0@walmart.com>
2021-05-20 12:45:07 -0700

    update

--- routes/root.js
+++ routes/root.js
@@ -4,9 +4,6 @@ const path = require("path");
 
 const ASSETS_DIR = path.join(__dirname, "../assets");
 
-const htmlText = fs.readFileSync(path.join(ASSETS_DIR, "index.html"), 'utf-8');
-const jsText = fs.readFileSync(path.join(ASSETS_DIR, "test-script.js"), 'utf-8');
-
 const csp = "script-src nonce-abc123";
 
 module.exports = async function (fastify, opts) {
@@ -15,9 +12,9 @@ module.exports = async function (fastify, opts) {
       .header("Content-Security-Policy-Report-Only", csp)
       .type("text/html")
       .code(200)
-      .send(htmlText);
+      .send(fs.readFileSync(path.join(ASSETS_DIR, "index.html"), 'utf-8'));
   });
   fastify.get("/test-script.js", async function (request, reply) {
-    reply.type("text/javascript").send(jsText);
+    reply.type("text/javascript").send(fs.readFileSync(path.join(ASSETS_DIR, "test-script.js"), 'utf-8'));
   });
 };
